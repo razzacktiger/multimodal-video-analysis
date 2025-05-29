3e7a6b7ca922b1b4cf8dacc2670b4c2280b6253c
Rayan Roshan <2006rayanroshan@gmail.com>
2025-05-29 11:07:49 -0400

    Removed the ai_processing function

--- video_analysis/src/App.jsx
+++ video_analysis/src/App.jsx
@@ -3,31 +3,6 @@ import './App.css'
 import { GoogleGenerativeAI } from "@google/generative-ai"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [inputValue, setInputValue] = useState('');
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-  const handleSubmit = async () => {
-    console.log('Submitted URL:', inputValue);
-    
-    const genAI = new GoogleGenerativeAI("");
-    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-    const result = await model.generateContent([
-      "Please create time-stamps for the video and make sure its in the format of 00:00 - title.",
-      {
-        fileData: {
-          fileUri: inputValue, // Use the input value from the user
-        },
-      },
-    ]);
-    const responseText = result.response ? await result.response.text() : 'No response received';
-    console.log(responseText);
-    setResponseText(responseText);
-  };
-
   return (
     <>
     <div>
